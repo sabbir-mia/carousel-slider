734ad24a407dd0d7f1e0438d1a6d73b53882097f
Sabbir Mia <73495673+sabbir-mia@users.noreply.github.com>
2023-09-16 13:01:17 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
+    //owl carousel
     $(".owl-carousel").owlCarousel({
         loop:true,
          margin:15,
          nav:true
          
     });
+
+    //wow.js.uk
+    new WOW().init();
 });
